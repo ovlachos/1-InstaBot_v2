c2915b451f5ea63f7939af68f34bf7cd3d0369c8
OrestisVlachos <ovlachos@gmail.com>
2020-09-21 20:14:26 +0300

    MinorThings2

--- py/playTheGame.py
+++ py/playTheGame.py
@@ -1,10 +1,15 @@
 import InstaFameBot as ib
 import auth
-from time import sleep
+import sys
+
+if len(sys.argv) > 1:
+    hdLess = sys.argv[1]
+else:
+    hdLess = False
 
 ib.log.error('\n\n')
 ib.log.error('~~<>~~ Let the Game Begin')
-gamingBot = ib.InstaBot(auth.username, auth.password)
+gamingBot = ib.InstaBot(auth.username, auth.password, headless=hdLess)
 gamingBot.tH_logIn()
 gamingBot.t2_theGame(30)
 print("~~<>~~ The Game is over")
