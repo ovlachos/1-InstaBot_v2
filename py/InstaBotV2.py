f71f32f5088e3ad36b34f8b14935f587d0b4c9c0
OrestisVlachos <ovlachos@gmail.com>
2020-10-14 22:57:23 +0300

    First steps

---
+++ py/InstaBotV2.py
@@ -0,0 +1,14 @@
+import auth
+from time import sleep
+from POM import webPage as wp
+from POM import insta_LogInPage_POM as login
+
+
+class FileHandler_Bot:
+    def __init__(self):
+        pass
+
+
+class InstaBot:
+    def __init__(self):
+        pass
